b5617b1625048ac0aeea20a97dfe7f5ff837478f
fajriyan <fajriyan20@gmail.com>
2024-02-05 20:17:00 +0700

    update:1.1.5 - fix layout payment link

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link
+        rel="icon"
+        href="https://raw.githubusercontent.com/fajriyan/simple-payment/main/public/favicon.ico"
+        sizes="any"
+      />
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/components/static/navbar.jsx
+++ src/components/static/navbar.jsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
@@ -6,14 +5,27 @@ const Navbar = () => {
     <div className="border-b mb-4">
       <div className="container mx-auto h-[60px] flex items-center justify-between">
         <Link href={"/"} className="font-bold">
-          Toko Buku
+          <img
+            width={35}
+            src={
+              "https://raw.githubusercontent.com/fajriyan/simple-payment/main/public/logo.png"
+            }
+          />
         </Link>
-        <Link
-          href={"/cart"}
-          className="border py-2 px-4 rounded-md hover:bg-slate-100"
-        >
-          Keranjang
-        </Link>
+        <div className="flex gap-2">
+          <Link
+            href={"/cart"}
+            className="border py-2 px-4 rounded-md hover:bg-slate-100"
+          >
+            Keranjang
+          </Link>
+          <Link
+            href={"/cart"}
+            className="border py-2 px-4 rounded-md hover:bg-slate-100"
+          >
+            Login
+          </Link>
+        </div>
       </div>
     </div>
   );
